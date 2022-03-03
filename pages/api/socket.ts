3a75686478176a7447b4206935e3997b72cc0dab
ZarNizza <gormaster@mail.ru>
2022-03-03 21:14:35 +0400

    tmp

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,47 +1,12 @@
 import { Server } from "Socket.IO";
+import type { Contract, ContractList } from "../index";
 
-type ContractList = {
-  id: string;
-  name: string;
-  removed?: boolean;
-};
-type Contract = {
-  contractId: string;
-  quote: {
-    price: number;
-    volume: number;
-  };
-};
-
-const ContractList: ContractList[] = [
+const conList: ContractList[] = [
   { id: "i-A", name: "AAAA", removed: false },
   { id: "i-B", name: "BBBB", removed: false },
   { id: "i-C", name: "CCCC", removed: false },
   { id: "i-D", name: "DDDD", removed: true },
   { id: "i-E", name: "EEEE", removed: false },
-];
-
-const Contract0: Contract[] = [
-  {
-    contractId: "i-A",
-    quote: { price: 1.1111, volume: 1111 },
-  },
-  {
-    contractId: "i-C",
-    quote: { price: 333.33, volume: 3333 },
-  },
-  {
-    contractId: "i-D",
-    quote: { price: 4444.4, volume: 4444 },
-  },
-  {
-    contractId: "i-B",
-    quote: { price: 22.222, volume: 2222 },
-  },
-  {
-    contractId: "i-E",
-    quote: { price: 55555.5, volume: 5555 },
-  },
 ];
 
 const c: [string, string, number][] = [
@@ -62,9 +27,8 @@ function genPool() {
   let arr: Contract[] = [];
   let j = Math.round(3 * Math.random()) + 1;
   while (j) {
-    j--;
     const k = Math.round(10 * Math.random());
-    console.log("generate k=", k);
+    console.log("j=", j, " generate k=", k);
 
     arr.push({
       contractId: c[k][0],
@@ -73,6 +37,7 @@ function genPool() {
         volume: Math.round(100000 * Math.random()),
       },
     });
+    j--;
   }
   return arr;
 }
@@ -87,14 +52,24 @@ const SocketHandler = (req, res) => {
     res.socket.server.io = io;
 
     io.on("connection", (socket) => {
+      // let listMsg = JSON.stringify(conList);
       socket.on("emitFlag", (f) => {
         let i = 3;
+        // if (f) {
+        //   socket.emit("dataList", conList);
+        //   console.log("OK, conList sended to frontend");
+        // }
+        console.log("inside S.emit, flag=", f, " i=", i);
+
         while (f && i) {
           i--;
           let msg = JSON.stringify(genPool());
-          console.log("socket.emit = ", msg);
-          socket.emit("dataFlow", msg);
-          setTimeout(() => console.log("...tick...", i, f), 2000);
+          console.log("...tick...", i, f);
+          // let msg = genPool();
+          setTimeout(() => {
+            console.log("send bit of Flow");
+            socket.emit("dataFlow", msg);
+          }, 2000);
         }
       });
     });
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,13 @@ import { CheckBtn } from "../components/CheckBtn";
 import io, { Socket } from "Socket.IO-client";
 import { DefaultEventsMap } from "@socket.io/component-emitter";
 
-let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
-type ContractList = {
+export type ContractList = {
   id: string;
   name: string;
   removed?: boolean;
 };
 
-type Contract = {
+export type Contract = {
   contractId: string;
   quote: {
     price: number;
@@ -20,72 +19,89 @@ type Contract = {
   awp?: number;
 };
 
+let socket: Socket<DefaultEventsMap, DefaultEventsMap>;
+
 const Home = () => {
   const ContractList0: ContractList = { id: "", name: "", removed: true };
   const Contract0: Contract = {
     contractId: "i-i",
     quote: { price: 0.101, volume: 1234 },
   };
-  const [conList, setConList] = useState<ContractList[]>([ContractList0]);
+  const [conList, setConList] = useState<ContractList[] | []>([]);
   const [ServEmitFlag, setServEmitFlag] = useState(false);
   const [awpFlag, setAwpFlag] = useState(true);
-  const [tabData, setTabData] = useState<Contract[]>([Contract0]);
+  const [tabData, setTabData] = useState<Contract[] | []>([]);
+  console.log("\n\n* * * * initial tabData=", tabData);
+  let tmpTD = [];
 
-  useEffect(() => socketInitializer(), []);
+  useEffect(() => {
+    async () => {
+      await fetch("/api/socket").finally(() => {
+        socket = io();
 
-  const socketInitializer: any = async () => {
-    await fetch("/api/socket");
-    socket = io();
+        socket.on("connect", () => {
+          console.log("connected");
+        });
 
-    socket.on("connect", () => {
-      console.log("connected");
-    });
+        // socket.on("dataList", (msg) => {
+        //   setConList(msg);
+        //   console.log("received conList=", conList);
+        // });
 
-    socket.on("dataFlow", (msg) => {
-      const mdata: Contract[] = JSON.parse(msg);
-      console.log("--------- received mData=", mdata);
-      let tmpTD = Array.from(tabData);
+        socket.on("dataFlow", (msg) => {
+          const mdata: Contract[] = JSON.parse(msg);
+          // const mdata: Contract[] = msg;
+          tmpTD = Array.from(tabData);
+          console.log("\n......... tabData =", tabData);
+          console.log("\n......... tmp =", tmpTD);
+          console.log("+++++++++ received mData=", mdata);
 
-      mdata.forEach((m) => {
-        const mindex = tabData.findIndex((t) => t.contractId === m.contractId);
-        m.awp = m.quote.price;
-        if (mindex === -1) {
-          tmpTD.push(m);
-        } else {
-          tmpTD[mindex] = m;
-        }
+          mdata.forEach((m) => {
+            const mindex = tmpTD.findIndex(
+              (t) => t.contractId === m.contractId
+            );
+            m.awp = m.quote.price;
+            if (mindex === -1) {
+              tmpTD.push(m);
+            } else {
+              tmpTD[mindex] = m;
+            }
+          });
+          console.log("======== setTabData=", tmpTD);
+          setTabData(() => tmpTD);
+        });
       });
-      console.log("======== setTabData=", tmpTD);
-      setTabData(() => tmpTD);
-    });
-  };
+    };
+  }, []);
 
   const servHandler = () => {
     setServEmitFlag(() => !ServEmitFlag);
     socket.emit("emitFlag", ServEmitFlag);
   };
 
-  function awpHandler() {
-    setAwpFlag(() => !awpFlag);
-  }
-
-  const Tabloid = (props) => {
-    console.log("props.data=", props.data);
+  const Tabloid = () => {
+    console.log("Tabloid - tabData=", tabData);
+    if (tabData.length === 0) return <p>No data yet...</p>;
     return (
       <div className={styles.tabloid}>
         <h3>TabData:</h3>
         <div className={styles.right}>
-          <CheckBtn text="Style" checked={awpFlag} onClick={awpHandler} />
+          <CheckBtn
+            text="Style"
+            checked={awpFlag}
+            onClick={setAwpFlag(() => !awpFlag)}
+          />
         </div>
 
         <ul>
-          {props.data.map((c: Contract) => (
+          {tabData.map((c: Contract) => (
             <li key={c.contractId}>
               <span className={styles.row}>
-                <span>{c.contractId}</span> <span>{c.quote.price}</span>
+                <span>{c.contractId}</span>{" "}
+                <span>{c.quote.price.toFixed(4)}</span>
               </span>
               <span className={awpFlag ? styles.awp : styles.noAwp}>
-                {c.awp || "-"}%
+                {(c.awp || 0).toFixed(2) || "-"}%
               </span>
             </li>
           ))}
@@ -100,7 +116,18 @@ const Home = () => {
         <input type="checkbox" onChange={servHandler} checked={ServEmitFlag} />
         ServEmitFlag
       </div>
-      <Tabloid data={tabData} />
+      {/* {conList.length > 0 ? (
+        <ul>
+          {conList.map((i) => (
+            <li key={i.id}>
+              {i.id} - {i.name} - {i.removed}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        ""
+      )} */}
+      <Tabloid />
     </div>
   );
 };
